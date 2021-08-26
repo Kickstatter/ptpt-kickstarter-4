17ac58e620d9d40a3e8f05f82a7ac96d1697d0f3
Lucas Hernandez <luke.hdez@gmail.com>
2021-08-25 21:43:03 -0400

    3rd commit

--- flask_app/kickstarter_dashboard.py
+++ flask_app/kickstarter_dashboard.py
@@ -2,3 +2,19 @@ from flask import Flask, request, render_template
 from flask_app.predict import kickstarter_prediction
 
 app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/data/', methods=['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"Wrong URL"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html', form_data=form_data)
+
+
+app.run()
